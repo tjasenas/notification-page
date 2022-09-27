cae0c4e90d56f933363978ecd6a74b532bc4d8a1
tjasenas <tomasjasenas@gmail.com>
2022-09-27 17:47:34 +0300

    add more styles

--- assets/js/small.js
+++ assets/js/small.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const selectBtn = document.querySelector('.btn-mark');
-const unreadedMsg = document.querySelectorAll('.notification__unreaded');
+const unreadedMsg = document.querySelectorAll('.still-unread');
 
 selectBtn.addEventListener('click', function(){
   unreadedMsg.forEach(function(e){
-    e.remove();
+    e.classList.remove('still-unread');
+    e.querySelector('.notification__unreaded').remove();
     document.querySelector('.head--unread').innerHTML = "0";
   });
 });
